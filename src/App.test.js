68fa8ce5e43386194398eb2681f616abb6894ca4
Abriel Cleaver <abrielcleaver@gmail.com>
2021-12-01 20:30:25 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,9 @@ import App from './App';
 
 test('renders the header and footer components', () => {
   render(<App />);
-  const header = screen.getByText(/Alchemy Blogger/i);
+  const header = screen.getByText(/QueerPlantMom's Blog Builder/i);
   expect(header).toBeInTheDocument();
 
-  const footer = screen.getByText(/Copyright Alchemy Codelab 2021-2022/i);
+  const footer = screen.getByText(/Copyright QueerPlantMom 2021-2022/i);
   expect(footer).toBeInTheDocument();
 });
